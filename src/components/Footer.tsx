1bdf609a5c52c940fd2bfa6d1f7642bd86a2ccaa
Marco Francisco de Oliveira Junior <167469636+Marc0l4@users.noreply.github.com>
2024-09-18 16:24:50 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ export const Footer = () => {
     return (
         <footer className="mt-5">
             <Separator />
-            <div className="my-5 text-center text-sm opacity-50">Feito por Marco Junior</div>
+            <div className="my-5 text-center text-sm opacity-50">
+                Feito por <a href="https://www.linkedin.com/in/marco-junior-7781472a8/">Marco Junior</a>
+            </div>
         </footer>
     );
 }
